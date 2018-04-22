2daa78d04441d76e8ab3e2a987696df95dc82a4d
sgg2123 <sgg2123@columbia.edu>
2018-04-22 23:39:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,11 +25,11 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].values.flatten
-  # return an array of all of the supplies that are used in the winter season
-
 end
 
+
 def all_supplies_in_holidays(holiday_hash)
+  
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
