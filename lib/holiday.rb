2eca5ed3d7b4492569a25d194a695ede91469549
sgg2123 <sgg2123@columbia.edu>
2018-04-22 23:56:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,9 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.colect do |season, holidays|
     holidays.select do |holiday, items|
       items.include?("BBQ")
+    end
     holiday_hash.keys.flatten
+  end
   
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
