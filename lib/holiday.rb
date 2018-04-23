14aa217f882889d081e40a7dec80cfefd0aa2712
sgg2123 <sgg2123@columbia.edu>
2018-04-23 00:04:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,6 +42,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holidays|
     holidays.select do |holiday, items|
       items.include?("BBQ")
-    end.keys
-  end.flatten
+    end
+  end.keys.flatten
 end
